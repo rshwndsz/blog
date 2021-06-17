c487addedbd71f008c79168ccac2914afa574b28
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2021-06-17 21:26:22 +0530

    Stylistic changes
    I'm back!

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,9 @@
-'use strict';
+"use strict";
 
-require('./src/assets/scss/init.scss');
-require('./static/css/prismjs/material-light.css');
+require("./src/assets/scss/init.scss");
+
+// require("prismjs/themes/prism-okaidia.css");
+// require("./static/css/prismjs/themes/atom-dark.css");
+// require("./static/css/prismjs/themes/shades-of-purple.css");
+// require("./static/css/prismjs/themes/vsc-dark-plus.css");
+require("./static/css/prismjs/themes/synthwave84.css");
