4a76ca54401e7581017aaf49ff3a67f799f5dee1
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2021-06-18 07:43:47 +0530

    Make prettier work with eslint

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,4 @@
 "use strict";
 
 require("./src/assets/scss/init.scss");
-
-// require("prismjs/themes/prism-okaidia.css");
-// require("./static/css/prismjs/themes/atom-dark.css");
-// require("./static/css/prismjs/themes/shades-of-purple.css");
-// require("./static/css/prismjs/themes/vsc-dark-plus.css");
 require("./static/css/prismjs/themes/synthwave84.css");
